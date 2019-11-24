719d95d7bffef83cc118cd1c3013c4f335014ec6
Prescott White <prgwhite@ucsc.edu>
2019-11-23 17:30:55 -0800

    Exit button calls finish() and kills MainActivity

--- app/src/main/java/com/cse118/movementdetector/MainActivity.java
+++ app/src/main/java/com/cse118/movementdetector/MainActivity.java
@@ -3,12 +3,27 @@ package com.cse118.movementdetector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonReset;
+    private Button mButtonExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonReset = findViewById(R.id.AM_button_reset);
+        mButtonExit = findViewById(R.id.AM_button_exit);
+
+        mButtonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
